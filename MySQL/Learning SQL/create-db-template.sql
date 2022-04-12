9f2584224efa45dcbf3a75e3bd601dff3e214917
yangoos57 <679oose@gmail.com>
2022-04-12 23:35:28 +0900

    2022-04-12

---
+++ MySQL/Learning SQL/create-db-template.sql
@@ -0,0 +1,10 @@
+CREATE Table person
+(
+    person_id SMALLINT UNSIGNED,
+    fname VARCHAR(20),
+    lname VARCHAR(20),
+    eye_color CHAR(20),
+    birth_date DATE,
+    street VARCHAR(30),
+    
+)
