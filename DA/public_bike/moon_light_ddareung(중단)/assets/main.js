651e95f7635167c6d8aa8523a82da69de7aa30e1
yangwoo lee <679oose@gmail.com>
2022-07-21 16:46:51 +0900

    자료정리

--- DA/public_bike/moon_light_ddareung(중단)/assets/main.js
+++
@@ -1,6 +0,0 @@
-const togglebtn = document.querySelector('.test_start');
-const target = document.querySelector('.test_target');
-
-togglebtn.addEventListener('click', ()=> {
-    target.classList.toggle('active');
-});
