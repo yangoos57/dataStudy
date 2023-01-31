f5041c77cefaa240fbd8c91d25bc207ac479480e
yangwoo lee <679oose@gmail.com>
2023-01-31 20:47:31 +0900

    2023-01-31

--- coding_test/programmers/[stack] 3번.py
+++ coding_test/programmers/[stack] 3번.py
@@ -7,15 +7,18 @@
 def solution(a, b):
     answer = []
     while a:
+        # 일단위 변화사항 체크
         for i in range(len(a)):
             a[i] += b[i]
 
+        # 성공이 100% 이상인 경우 모두 뽑아내기
         c = 0
         while a and a[0] >= 100:
             a.pop(0)
             b.pop(0)
             c += 1
 
+        # 1개라도 있다면 answer에 append 하기
         if c > 0:
             answer.append(c)
 
@@ -26,3 +29,46 @@ a = [1, 1, 50]
 b = [100, 2, 1]
 
 print(solution(a, b))
+
+# ---
+
+### 23.1.31. 다시 품
+# QA의 중요성을 다시 한 번 꺠닫는다.
+# while 문 안에 else 항목의 x = y 를 y = x로 썼다가 원인 찾느라 고생했음
+# 코드를 에러없이 쓰게하기 위해선 개별 단위를 점검 할 수 있는 방법에 대해서 고민해야할듯
+# if else 문을 쓰면 값들이 나눠져서 흐르느끼 둘 다 점검 해야하는 것을 잊지 말자
+
+
+def solution(p, s):
+    from math import ceil
+
+    # 개별 값의 완성 날짜를 계산
+    finish = []
+    for i in range(len(p)):
+        rest = 100 - p[i]
+        days = ceil(rest / s[i])
+        finish.append(days)
+
+    # stack을 쓰기 위해 reversed
+    finish = list(reversed(finish))
+    print(finish)
+
+    # 기준 되는 값 생성
+    x = finish.pop()  # 7
+
+    deploy = []
+    v = 1
+    while finish:
+        y = finish.pop()
+        if x >= y:  # 3
+            v += 1
+        else:
+            deploy.append(v)
+            # 기준 변경하고 새로 생성
+            x = y
+            v = 1
+
+    # 마지막 배포도 저장
+    deploy.append(v)
+
+    return deploy
